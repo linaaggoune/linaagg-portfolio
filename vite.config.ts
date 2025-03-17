274641185deba7369d69680295a5439e7c708d91
lina <lina.aggn@gmail.com>
2025-03-17 13:24:09 +0100

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/linaagg-portfolio",
+  base: "/",
   plugins: [react(), tsconfigPaths()],
 });
